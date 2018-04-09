688666ee701a42503060327ec94613457bb29ec5
Jackson Ndiho <jackson.ndiho@gmail.com>
2018-04-09 12:30:10 -0500

    exercise 2 completed

--- static.js
+++ static.js
@@ -1,4 +1,7 @@
-express = require("express");
-
+var express = require("express");
+var path = require('path');
 var app = express();
 
+app.use(express.static(process.argv[3]||path.join(__dirname, 'public')));
+
+app.listen(process.argv[2]);
